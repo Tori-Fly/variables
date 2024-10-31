ea6eae9896e163e5ef74a89daaccb7c7d3bc3358
Tori-Fly <paiette511@gmail.com>
2024-10-31 10:41:51 +0300

    Add files via upload
    Переменные

---
+++ variables.py
@@ -0,0 +1,6 @@
+number_of_works_performed = 12
+number_of_hours_spent = 1.5
+course = "Python"
+time_per_task = number_of_hours_spent / number_of_works_performed
+print("Курс:", course, "всего задач:", number_of_works_performed, "затрачено часов:", number_of_hours_spent,
+      "среднее время выполнения", time_per_task, "часа")
